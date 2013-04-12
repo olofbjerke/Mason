594c9b4002a48ed0d961a1e80a968f351bd69727
Olof Bjerke <olofbjerke@gmail.com>
2013-04-12 23:18:18 +0200

    Moved the main code into a jQuery plugin called .mason()

--- js/mason.js
+++ js/mason.js
@@ -1,106 +1,113 @@
-function get_rand_color()
-{
-	var color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
-	while(color.length < 6)
-		color = "0" + color;
-	return "#" + color;
-}
+/*
+	Mason JS
+ */
+(function( $ ){
 
-function masonry (className) {
+	$.fn.mason = function() {
 
-	var images			= [];	// Array with all images
-	var row				= [];	// Current row containing images
-	var current			= 0;	// Current image of all images
-	var rowWidth		= 0;	// combined width of images in a row
-	var step			= 1;	// how many pixels the height is changed by
-	var diff			= 300;	// Images +- diff is added even if combined width is more than columnWidth 
-	var border			= 50;	// Border + margin around an image
-	var columnWidth		= $(className).width() - 12; // Width of the column
-	var windowHeight	= $(window).height(); // Height of the viewport for standard size of the images
+		var images			= [];	// Array with all images
+		var row				= [];	// Current row containing images
+		var current			= 0;	// Current image of all images
+		var rowWidth		= 0;	// combined width of images in a row
+		var step			= 1;	// how many pixels the height is changed by
+		var diff			= 300;	// Images +- diff is added even if combined width is more than columnWidth
+		var border			= 50;	// Border + margin around an image
+		var columnWidth		= this.width() - 12; // Width of the column
+		var windowHeight	= $(window).height(); // Height of the viewport for standard size of the images
 
-	// This does not work in firefox by some reason
-	// var border			= 0 + (parseInt($(classNameimg:first').css('border-width'), 10) +
-	//	parseInt($(classNameimg:first').css('margin-left'), 10)) * 2;
+		// This does not work in firefox by some reason
+		// var border			= 0 + (parseInt($(classNameimg:first').css('border-width'), 10) +
+		//	parseInt($(classNameimg:first').css('margin-left'), 10)) * 2;
 
-	// Set the same height for every image
-	$(className + " img").height(windowHeight / 3);
 
-	// save all images in an array
-	$(className + " img").each(function() {
-		images.push($(this));
-	});
+		// Fix the height and width of the row
+		function fixate (row, targetWidth, step) {
 
-	// Go through all images and create rows
-	while(current < images.length)
-	{
-		//Current row width
-		rowWidth += images[current].width() + border;
-		row.push(images[current]);
+			var height		= 0;
+			var rowWidth	= 0;
+			// This border does not work in firefox
+			var border		= 0 + (parseInt(row[0].css('border-width'), 10) +
+				parseInt(row[0].css('margin-left'), 10)) * 2;
 
-		// More images in array and one more can be added to the current row?
-		if(((current + 1) != images.length) && (rowWidth + images[(current + 1)].width() + border) < (columnWidth + diff))
+			// Set up the current row width
+			for(var i = 0; i < row.length; i++)
+			{
+				rowWidth += row[i].width() + border;
+			}
+
+			// Row needs to be become smaller
+			if((rowWidth > targetWidth))
+			{
+				// Is it wide enough?
+				while(rowWidth > targetWidth)
+				{
+					// Width of the row images together is reset after each iteration
+					rowWidth = 0;
+					// The new height
+					height = row[0].height() - step;
+					// Go through all images in the row
+					for(i = 0; i < row.length; i++)
+					{
+						row[i].height(height);
+						rowWidth += row[i].width() + border;
+					}
+				}
+			}
+			// Row needs to be higher and wider
+			else if((rowWidth < targetWidth))
+			{
+				// Is it wide enough?
+				while(rowWidth < targetWidth)
+				{
+					// Width of the row images together is reset after each iteration
+					rowWidth = 0;
+					// The new height
+					height = row[0].height() + step;
+					// Go through all images in the row
+					for(i = 0; i < row.length; i++)
+					{
+						row[i].height(height);
+						rowWidth += row[i].width() + border;
+					}
+				}
+			}
+		}
+
+		// Set the same height for every image
+		this.find("img").height(windowHeight / 3.2);
+
+		// save all images in an array
+		this.find("img").each(function() {
+			images.push($(this));
+		});
+
+
+		// Go through all images and create rows
+		while(current < images.length)
 		{
+			//Current row width
+			rowWidth += images[current].width() + border;
+			row.push(images[current]);
+
+			// More images in array and one more can be added to the current row?
+			if(((current + 1) !== images.length) &&
+				(rowWidth + images[(current + 1)].width() + border) < (columnWidth + diff))
+			{
+				current++;
+				continue;
+			}
+
+			// Fix the width and height of the row
+			fixate(row, columnWidth, step);
+
+			// Reset row
+			row			= [];
+			rowWidth	= 0;
+			// Go to next image
 			current++;
-			continue;
 		}
-
-		fixate(row, columnWidth, step);
-
-		// Reset row
-		row			= [];
-		rowWidth	= 0;
-		// Go to next image
-		current++;
-	}
-}
-
-function fixate (row, targetWidth, step) {
-	// This border does not work in firefox
-	var border		= 0 + (parseInt(row[0].css('border-width'), 10) +
-		parseInt(row[0].css('margin-left'), 10)) * 2;
-	var rowWidth	= 0;
-
-	// Set up the current row width
-	for(var i = 0; i < row.length; i++)
-		rowWidth += row[i].width() + border;
-
-	// Row needs to be become smaller
-	if((rowWidth > targetWidth))
-	{
-		// Is it wide enough?
-		while(rowWidth > targetWidth)
-		{
-			// Width of the row images together is reset after each iteration
-			rowWidth = 0;
-			// The new height
-			h = row[0].height() - step;
-			// Go through all images in the row
-			for(i = 0; i < row.length; i++)
-			{
-				row[i].height(h);
-				rowWidth += row[i].width() + border;
-			}
-		}
-	}
-	// Row needs to be higher and wider
-	else if((rowWidth < targetWidth))
-	{
-		// Is it wide enough?
-		while(rowWidth < targetWidth)
-		{
-			// Width of the row images together is reset after each iteration
-			rowWidth = 0;
-			// The new height
-			h = row[0].height() + step;
-			// Go through all images in the row
-			for(i = 0; i < row.length; i++)
-			{
-				row[i].height(h);
-				rowWidth += row[i].width() + border;
-			}
-		}
-	}
-}
+	};
+})( jQuery );
 
 jQuery(document).ready(function() {
 	$('.popup').hide();
@@ -108,7 +115,7 @@ jQuery(document).ready(function() {
 
 jQuery(window).load(function() {
 
-	masonry(".masonry");
+	$(".masonry").mason();
 
 	$('.masonry a').click(function(event)
 	{
