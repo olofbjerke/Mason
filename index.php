cdb60081191150e85934aa065a661aa198ebd62f
Olof Bjerke <olofbjerke@gmail.com>
2013-04-12 16:06:05 +0200

    Added larger photos and renamed some files

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 	<meta charset="utf-8">
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" href="styles/karbinerochsolskydd.css">
-	<title>Olof Bjerke - Karbiner och solskydd</title>
+	<link rel="stylesheet" href="styles/mason.css">
+	<title>Mason JS</title>
 </head>
 <body>
 	<header>
 		<div>
 			<hgroup>
-				<h1>Karbiner och solskydd</h1>
-				<h2>Olof och Frida i Thailand 2013</h2>
+				<h1>Mason JS</h1>
+				<h2>Display multiple images with various proportions in rows of equal height and width.</h2>
 			</hgroup>
 		</div>
 	</header>
@@ -23,7 +23,7 @@
 				<? if ($handle = opendir('img/album/thailand_2013/thumbs/')):
 					while (false !== ($entry = readdir($handle))):
 						if ($entry != "." && $entry != ".."): ?>
-							<a href="img/album/thailand_2013/thumbs/<?= $entry ?>">
+							<a href="img/album/thailand_2013/large/<?= $entry ?>">
 								<img src="img/album/thailand_2013/thumbs/<?= $entry ?>" class="brick" alt="">
 							</a>
 				<?	
@@ -38,7 +38,7 @@
 	<div class="popup"></div>
 	<footer>
 		<script type="text/javascript" src="js/jquery-1.9.0.min.js"></script>
-		<script type="text/javascript" src="js/karbinerochsolskydd.js"></script>
+		<script type="text/javascript" src="js/mason.js"></script>
 	</footer>
 </body>
 <html>
