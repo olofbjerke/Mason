dd2f90eb5522c77f2e520863a7f62fde5188ba75
Olof Bjerke <olofbjerke@gmail.com>
2013-04-18 09:15:26 +0200

    Merged version from branch develop

--- js/jquery.mason.js
+++ js/jquery.mason.js
@@ -3,38 +3,53 @@
    Mason JS
    --------------------------------
    + https://github.com/olofbjerke/Mason
-   + version 0.5
-   + Copyright 2013 Olof Bjerke
+   + version 0.8
+   + Copyright 2013 Olof Bjerke & Rostislav Raykov
    + Licensed under the MIT license
 */
-(function( $ ){
+(function($) {
+	$.fn.mason = function(opts) {
+		var options = $.extend({},
+		{
+			onResizeStart:            function() {},
+			onResizeEnd:              function() {},
+			onResizeEndBeforeMasonry: function() {},
+			onResizeEndAfterMasonry:  function() {}
+		},
+			opts || {}
+		);
 
-	$.fn.mason = function() {
+		var $this				= $(this);				// To be able to address the masonry
+		var images				= [];					// Array with all images
+		var row					= [];					// Current row containing images
+		var rowWidth			= 0;					// Used to calculate the row width
+		var columnWidth			= $this.width() - 1;	// 1 is subtracted for subpixel problems
+		var windowWidth			= window.innerWidth;	// Look for resizes
+		var imageDenominator	= 3.5;					// Divided of
+		var border				= 0;					// Border, padding and margin. Calculated in collect
+		var resizeSingles		= true;					// Set to false if single image rows should not scale
 
-		var images			= [];	// Array with all images
-		var row				= [];	// Current row containing images
-		var current			= 0;	// Current image of all images
-		var rowWidth		= 0;	// combined width of images in a row
-		var step			= 1;	// how many pixels the height is changed by
-		var diff			= 300;	// Images +- diff is added even if combined width is more than columnWidth
-		var border			= 0;	// Border + margin around an image
-		var columnWidth		= this.width(); // Width of the column
-		var windowHeight	= $(window).height(); // Height of the viewport for standard size of the images
+		var collect = function() {
+			$this.find('img').each(function(){
+				images.push($(this));
+				height = Math.floor(window.innerHeight / imageDenominator);
+				$(this).height(height)
+					.css({display: 'block', float: 'left'})
+					.data('top', $(this).position().top);
+			});
+			border = images[0].outerWidth(true) - images[0].width();
+		};
 
-		// Fix the height and width of the row
-		function fixate (row, targetWidth, step) {
+		var resize_row = function() {
+			var rowRatio	= 0;
+			rowWidth = 0;
 
-			var height			= 0;
-			var rowWidth		= 0;
-			var rowWidth2		= 0;
-			var rowRatio		= 0;
-			var distanceLeft	= 0;
-			targetWidth			-= 1; // Fix for sub pixel rendering issues
-			var border			= 0 + row[0].outerWidth(true) - row[0].width();
-
-			// Fix width if row has only one image
-			if(row.length === 1)
-			{
+			// don't resize if only one image on the row
+			if (row.length == 1) {
+				if(resizeSingles)
+				{
+					row[0].width(columnWidth - border).height("auto");
+				}
 				return;
 			}
 
@@ -45,57 +60,91 @@
 			}
 
 			// The new height for all images in the row
-			height = Math.floor((targetWidth - border * row.length) / rowRatio);
+			height = Math.floor((columnWidth - border * row.length) / rowRatio);
 
 			for(i = 0; i < row.length; i++)
 			{
 				row[i].height(height);
-				w = row[i].width();
-				row[i].width(w);
-				rowWidth += row[i].width() + border;
+				w = row[i].width();		// This is done to get a int value as a width and not a float
+				row[i].width(w);		// This takes a lot of time
+				rowWidth += w + border;	// Counting the row width for the fix at the end
 			}
 
 			// Fix last image to get better row endings.
-			if(rowWidth != targetWidth)
+			if(rowWidth != columnWidth)
 			{
-				width = row[(row.length - 1)].width() + ((targetWidth - rowWidth));
+				width = row[(row.length - 1)].width() + ((columnWidth - rowWidth));
 				row[(row.length - 1)].width(width);
 			}
 
-		}
+		};
 
-		// Set the same height for every image
-		this.find("img").height(windowHeight / 3.2);
+		var masonry = function() {
+			var prevPosition  = images[0].data('top');
+			var position      = prevPosition;
+			var totalImages   = images.length;
+			var currentImg    = 0;
+			var rowCounter    = 0;
 
-		// save all images in an array
-		this.find("img").each(function() {
-			images.push($(this));
-		});
+			while(currentImg < totalImages) {
+				position = images[currentImg].data('top');
 
-		border			= 0 + images[0].outerWidth(true) - images[0].width();
-		// Go through all images and create rows
-		while(current < images.length)
-		{
-			//Current row width
-			rowWidth += images[current].width() + border;
-			row.push(images[current]);
+				if (prevPosition != position)
+				{
+					resize_row();
 
-			// More images in array and one more can be added to the current row?
-			if(((current + 1) !== images.length) &&
-				(rowWidth + images[(current + 1)].width() + border) < (columnWidth + diff))
-			{
-				current++;
-				continue;
+					// Reset row
+					row			= [];
+					rowCounter	= 0;
+					rowWidth   = 0;
+				}
+
+				//Current row width
+				rowWidth         += images[currentImg].outerWidth(true);
+				row[rowCounter++] = images[currentImg];
+				prevPosition      = position;
+
+				// Go to next image
+				currentImg++;
 			}
-
-			// Fix the width and height of the row
-			fixate(row, columnWidth, step);
-
-			// Reset row
+			resize_row();
 			row			= [];
+			rowCounter	= 0;
 			rowWidth	= 0;
-			// Go to next image
-			current++;
-		}
+			currentImg	= 0;
+		};
+
+		var resetHeight = function () {
+			for(i = 0; i < images.length; i++)
+			{
+				images[i].height(window.innerHeight/ imageDenominator).
+					width("auto").data('top', images[i].position().top);
+			}
+		};
+
+		return this.each(function(){
+			collect();
+			masonry();
+
+			var resizeTimer;
+			$(window).resize(function(){
+				options.onResizeStart.call($this);
+
+				// don't trigger resize event unless the resize has stopped 
+				// for at least half a second and the window width is different
+				clearTimeout(resizeTimer);
+				resizeTimer = setTimeout(function() {
+					options.onResizeEnd.call($this);
+
+					columnWidth	= $this.width() - 1;
+					windowWidth = window.innerWidth;
+
+					options.onResizeEndBeforeMasonry.call($this);
+					resetHeight();
+					masonry();
+					options.onResizeEndAfterMasonry.call($this);
+				}, 500);
+			});
+		});
 	};
-})( jQuery );
+})(jQuery);
